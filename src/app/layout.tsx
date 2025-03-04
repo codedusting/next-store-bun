8b47b575b2ae7bb8ceb18d21fb45ac82a62c087e
Code Dusting <codedusting@gmail.com>
2025-03-04 15:18:13 +0000

    Upgraded packages + Shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 
+import { ToastContainer, Flip } from "react-toastify";
+
 import "./globals.css";
 
 const lexend = Lexend({ subsets: ["latin"], display: "swap", variable: "--font-lexend" });
@@ -17,7 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${lexend.variable} font-sans antialiased`}>{children}</body>
+      <body className={`${lexend.variable} font-sans antialiased`}>
+        <main>{children}</main>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Flip}
+        />
+      </body>
     </html>
   );
 }
